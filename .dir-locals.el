427a0f9ba339aeb788b8cfb990b4a39a11977f6d
Dan Fornika <dfornika@gmail.com>
2023-08-23 18:39:45 -0700

    Prepare for gh-pages deployment

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,3 @@
-((clojurescript-mode . ((cider-clojure-cli-aliases . "-A:dev")
+((clojurescript-mode . ((cider-clojure-cli-aliases . "-A:dev:fig")
 			(cider-default-cljs-repl . figwheel-main)
 			(cider-figwheel-main-default-options . ":dev"))))
---
+++ scripts/create-deployment.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -r target/deploy/genomics-qc 2> /dev/null
+mkdir -p target/deploy/genomics-qc
+mkdir -p target/deploy/genomics-qc/js
+cp -r resources/public/css target/deploy/genomics-qc
+cp -r resources/public/data target/deploy/genomics-qc
+cp -r resources/public/images target/deploy/genomics-qc
+cp target/public/cljs-out/prod/main_bundle.js target/deploy/genomics-qc/js/main.js
+cp resources/public/index_prod.html target/deploy/genomics-qc/index.html
+pushd target/deploy > /dev/null
+tar -czf genomics-qc.tar.gz genomics-qc
+popd > /dev/null
