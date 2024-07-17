c1e68af3c77f7a813354513ca29d4e54b1583741
Dan Fornika <dfornika@gmail.com>
2024-07-17 12:09:44 -0700

    Adopt shadow-cljs, drop Figwheel

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,8 @@
-((clojurescript-mode . ((cider-clojure-cli-aliases . "-A:dev:fig")
-			(cider-default-cljs-repl . figwheel-main)
-			(cider-figwheel-main-default-options . ":dev"))))
+((nil . ((cider-clojure-cli-aliases . ":dev")
+	 (cider-preferred-build-tool . clojure-cli)
+	 (cider-default-cljs-repl . custom)
+	 (cider-custom-cljs-repl-init-form . "(do (user/cljs-repl))")
+	 (eval . (progn
+		   (make-variable-buffer-local 'cider-jack-in-nrepl-middlewares)
+		   (add-to-list 'cider-jack-in-nrepl-middlewares "shadow.cljs.devtools.server.nrepl/middleware"))))))
+
