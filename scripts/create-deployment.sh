96d22c5c34469e47b2c33d0f758b708cd4e25373
Dan Fornika <dfornika@gmail.com>
2024-08-22 09:34:11 -0700

    Update build scripts and workflow

--- scripts/create-deployment.sh
+++ scripts/create-deployment.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-rm -r target/deploy/genomics-qc 2> /dev/null
-mkdir -p target/deploy/genomics-qc
-mkdir -p target/deploy/genomics-qc/js
-cp -r resources/public/css target/deploy/genomics-qc
-cp -r resources/public/data target/deploy/genomics-qc
-cp -r resources/public/images target/deploy/genomics-qc
-cp target/public/cljs-out/prod/main_bundle.js target/deploy/genomics-qc/js/main.js
-cp resources/public/index_prod.html target/deploy/genomics-qc/index.html
-pushd target/deploy > /dev/null
+rm -r build/genomics-qc 2> /dev/null
+
+mkdir -p build/genomics-qc/js
+
+cp -r resources/public/css build/genomics-qc
+cp -r resources/public/data build/genomics-qc
+cp -r resources/public/images build/genomics-qc
+cp    resources/public/js/main.js build/genomics-qc/js/main.js
+cp    resources/public/index.html build/genomics-qc/index.html
+cp    resources/public/favicon.ico build/genomics-qc/favicon.ico
+
+pushd build > /dev/null
 zip genomics-qc genomics-qc/*
 popd > /dev/null
